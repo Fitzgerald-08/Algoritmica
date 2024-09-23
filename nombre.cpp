0925c0eabce20fcfd2e9a2544637cbc8433084fc
Fitzgerald-08 <122659902+Fitzgerald-08@users.noreply.github.com>
2024-09-22 19:35:42 -0700

    Add files via upload

--- nombre.cpp
+++ nombre.cpp
@@ -1,17 +1,25 @@
-/* #include <conio.h>
-#include <stdio.h> */
-#include <cstdio> /* Las dos librerias de arriba fueron sustituidas por <cstdio> */
+#include <iostream>
 
 using namespace std;
 
 int main(){
     char nombre[20], apellido[20];
 
-    printf("Introduzca su nombre: ");
+    cout << "Introduzca su nombre: ";
+    cin >> nombre;
+
+    cout << "Introduzca su apellido: ";
+    cin >> apellido;
+
+    cout << "Hola " << nombre << " " << apellido;
+
+
+
+    /* printf("Introduzca su nombre: ");
     scanf("%s", nombre);
     printf("Introduzca su apellido: ");
     scanf("%s", apellido);
-    printf("Hola %s %s, buenos d%cas.", nombre, apellido, 161); /* El numero 161 es un codigo para el caracter í */
+    printf("Hola %s %s, buenos días.", nombre, apellido); */
 
     return 0;
 }
